580566a91df49884c1881e356ecc029660ecc150
ab3MN <ab3mn3@gmail.com>
2020-09-24 20:34:22 +0300

    Updates

---
+++ precache-manifest.dba7e82c4e8d52516d390a71da61e25a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b693b45db7a7d118f7649089ce7b6dbd",
+    "url": "/JustDoIt/index.html"
+  },
+  {
+    "revision": "5c2250eb259efe777a62",
+    "url": "/JustDoIt/static/css/main.02e609fd.chunk.css"
+  },
+  {
+    "revision": "3eadb00cb5eac7380fc8",
+    "url": "/JustDoIt/static/js/2.8a9c63f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JustDoIt/static/js/2.8a9c63f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c2250eb259efe777a62",
+    "url": "/JustDoIt/static/js/main.7af64d9e.chunk.js"
+  },
+  {
+    "revision": "4b63484b2fa87a75161f",
+    "url": "/JustDoIt/static/js/runtime-main.ee1e56d0.js"
+  }
+]);
